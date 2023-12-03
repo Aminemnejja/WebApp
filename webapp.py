a1f40cfb4983d2094bb544dd4e349db29ac89f9a
unknown <mnejjaamine4@gmail.com>
2023-12-03 15:24:00 +0100

    Description de vos modifications

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ def run_flask():
     subprocess.run(["python", "html_flask.py"])
 
 def run_streamlit():
-    subprocess.run(["streamlit", "run", "menu.py","--server.port", "8081"])
+    subprocess.run(["streamlit", "run", "menu.py"])
     
 # Lancer Flask dans un thread séparé
 flask_thread = threading.Thread(target=run_flask)
