a9dbf90b49785bc783cf835215545b38e0751bed
unknown <mnejjaamine4@gmail.com>
2023-12-03 16:45:33 +0100

    Description de vos modifications

--- html_flask.py
+++ html_flask.py
@@ -43,4 +43,5 @@ def update_vector_values():
     return jsonify({'message': 'Vector values updated successfully!'})
 
 if __name__ == '__main__':
- app.run(debug=True)
+ app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
